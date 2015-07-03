3230cb8f2180278d84c6679b542a0608d1d3eb11
Marius Muk <riamarius@gmail.com>
2015-07-02 20:56:50 -0400

    Added initial wareframe

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// app js script
+
+//make sure we don't execute any script before the document is fully loaded
+$(document).ready(function(){
+	console.log('app is ready!');
+
+	// add event listner to add button
+	$('#add-button').click(function(){
+		var item = $('#shopping-item').val();
+		console.log('Add new item: '+item);
+		$('#shopping-item-list tbody').append('<tr><td>new</td><td>bread</td><td>1</td><td>Trader Joe</td></tr>');
+	})
+
+	// remove item from the list
+	$('#remove-button').click(function(){
+		console.log('removing item..');
+		$('#shopping-item-list tbody tr:last').remove();
+	})
+})
